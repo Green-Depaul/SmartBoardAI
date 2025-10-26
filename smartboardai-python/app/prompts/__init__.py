28a733821047124d3a4861b3cc58cc3978ca1777
cuellern <121742960+cuellern@users.noreply.github.com>
2025-10-26 17:38:59 -0500

    Delete smartboardai-python directory

--- smartboardai-python/app/prompts/__init__.py
+++
@@ -1,9 +0,0 @@
-# Prompts module for SmartBoardAI
-# Contains all system prompts and prompt templates
-
-from .system_prompts import SystemPrompts
-from .templates import PromptTemplates
-from .constants import PromptConstants
-
-__all__ = ['SystemPrompts', 'PromptTemplates', 'PromptConstants']
-
